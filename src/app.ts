b9d0869bfe452b1010c9437bf4d5552e3a2bb814
asifaowadud <asifaowadud@gmail.com>
2026-01-19 12:40:55 +0600

    feat: limit allowed

--- src/app.ts
+++ src/app.ts
@@ -38,8 +38,9 @@ app.use(
           credentials: true,
      }),
 );
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use(express.json({ limit: '100mb' }));
+app.use(express.urlencoded({ extended: true, limit: '100mb' }));
 
 // Session configuration for OAuth
 app.use(
