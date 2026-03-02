2cb996e09bfcf92dc4c620cf0ba92f2eab816b2e
Asif A Owadud <72104020+abuabddullah@users.noreply.github.com>
2026-03-02 11:14:03 +0600

    mnd

--- src/app/modules/user/user.service.ts
+++ src/app/modules/user/user.service.ts
@@ -48,19 +48,22 @@ const createUserToDB = async (payload: { name: string; contact: string; role: US
                }
           );
 
-          // Send OTP
+          // // Send OTP
           // await sendSMS(user!.contact!, `Your OTP is ${otp}`);
 
           // // ✅ Get user with authentication
           // const userWithAuth = await User.findById(user!._id).select('+authentication');
 
+          // const userResponse: any = userWithAuth?.toObject();
+          // if (userResponse?.authentication) {
+          //      userResponse.authentication = {
+          //           oneTimeCode: userResponse.authentication.oneTimeCode,
+          //           expireAt: userResponse.authentication.expireAt
+          //      };
+          // }
+          
           const userResponse: any = user.toObject();
-          if (userResponse?.authentication) {
-               userResponse.authentication = {
-                    oneTimeCode: userResponse.authentication.oneTimeCode,
-                    expireAt: userResponse.authentication.expireAt
-               };
-          }
+          userResponse.authentication = authentication;
 
           return userResponse;
 
